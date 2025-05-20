2c3f17a4d96821b5f9a02ff44adb3980b2c87557
Angel <angel.alcaraz.c@gmail.com>
2025-05-20 17:18:12 +0000

    complete lab 13 more component communication.
    complete lab 13 more component communication.

--- daily-work/react/day-7/projectpilot/src/projects/ProjectForm.tsx
+++ daily-work/react/day-7/projectpilot/src/projects/ProjectForm.tsx
@@ -1,4 +1,9 @@
-function ProjectForm() {
+interface ProjectFormProps {
+    onCancel: () => void;
+}
+
+
+function ProjectForm({onCancel}: ProjectFormProps ) {
     return (
         <form className="input-group vertical">
             <label htmlFor="name">Project Name</label>
@@ -8,7 +13,7 @@ function ProjectForm() {
             <textarea id="description" name="description" placeholder="enter description" autoComplete="off" />
 
             <label htmlFor="budget">Project Budget</label>
-            <input id="budget" name="budget" type="number" placeholder="enter budget" autoComplete="off"/>
+            <input id="budget" name="budget" type="number" placeholder="enter budget" autoComplete="off" />
 
             <label htmlFor="isActive">Active?</label>
             <input id="isActive" name="isActive" type="checkbox" />
@@ -16,7 +21,7 @@ function ProjectForm() {
             <div className="input-group">
                 <button className="primary bordered medium">Save</button>
                 <span></span>
-                <button type="button" className="bordered medium">Cancel</button>
+                <button type="button" className="bordered medium" onClick={onCancel}>Cancel</button>
             </div>
         </form>
     );
