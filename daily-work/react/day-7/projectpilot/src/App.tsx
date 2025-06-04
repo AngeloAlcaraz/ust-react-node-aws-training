22e537aedbf3bb52d1f264285e58254930223a06
Angel <angel.alcaraz.c@gmail.com>
2025-06-04 15:32:49 +0000

    fix import paths.
    fix import paths.

--- daily-work/react/day-7/projectpilot/src/App.tsx
+++ daily-work/react/day-7/projectpilot/src/App.tsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import { useState } from "react";
-import HomePage from "./components/home/home-component";
+import HomePage from "./components/home/HomePage";
 import ProjectsPage from "./components/projects/ProjectsPage";
 import ProjectPage from './components/projects/ProjectPage';
-import NewProjectPage from "./components/projects/add-new-project.component";
+import NewProjectPage from "./components/projects/newProjectPage";
 import SearchBar from "./components/projects/SearchBar";
 import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
 
--- daily-work/react/day-7/projectpilot/src/components/projects/newProjectPage.tsx
+++ daily-work/react/day-7/projectpilot/src/components/projects/newProjectPage.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import ProjectForm from "./ProjectForm";
 import { Project } from "../../models/Project";
-import { ProjectAPI } from "../../services/projectAPI"
+import { ProjectAPI } from "../../services/project-service";
 import { useNavigate } from "react-router";
 
 const createEmptyProject = () => new Project({
